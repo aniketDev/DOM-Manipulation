d93028205d95e8df807334171fcfe170ce060468
Aniket Mandal <aniket@balihans.com>
2019-02-20 15:52:38 +0530

    secong commit

--- dom1.js
+++ dom1.js
@@ -13,7 +13,8 @@
 // console.log(document.forms);
 // console.log(document.images);
 
-// getElementById //
+
+//****** getElementById ************//
 
 let getItem = document.getElementById('header-text');
 // console.log(getItem.innerText);
@@ -22,3 +23,37 @@ let getItem = document.getElementById('header-text');
 // getItem.innerText = "hi";
 // getItem.innerHTML = "<h3>Hello</h3>";
 // getItem.style.display = "none";
+
+
+//********** getElementByClassName ********//
+
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'hello';
+// items[1].style.backgroundColor = 'grey';
+
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = '700';
+// }
+
+
+//*********** getElementByTagName *************//
+
+// let li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'hello';
+// li[1].style.backgroundColor = 'grey';
+
+// for (let i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = '700';
+// }
+
+
+//*********** querySelecter *************//
+
+let header = document.querySelector('#header'); // any css selecter just like jQuery
+// console.log(header);
+// header.style.display = 'none';
+
